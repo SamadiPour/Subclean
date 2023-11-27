c46bbb64b077081275e0f7970516e6b7da0e0ecf
Amir Hossein <samadipoor2@gmail.com>
2023-11-27 21:32:39 +0330

    feat: first version - initial

---
+++ subclean.user.js
@@ -0,0 +1,80 @@
+// ==UserScript==
+// @name         Subclean
+// @namespace    http://github.com/SamadiPour
+// @author       http://github.com/SamadiPour
+// @version      1.0
+// @description  Subscene subtitle list cleaner
+// @match        https://subscene.com/subtitles/*
+// @icon         https://subscene.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+const showAll = true; // make it false to only keep one
+const cleanText = true; // make it false if you don't want to see changes in the Release Name/Film title
+
+
+(function() {
+    'use strict';
+
+    function removeDuplicates() {
+        const uniqueElements = {};
+
+        // group duplicates
+        const rows = document.getElementsByTagName('table')[0].querySelectorAll('tr');
+        rows.forEach((row) => {
+            const anchorElement = row.querySelector('td.a1 a');
+            if (anchorElement) {
+                const href = anchorElement ? anchorElement.getAttribute('href') : null;
+                if (href && !uniqueElements[href]) {
+                    uniqueElements[href] = row;
+                } else {
+                    if (showAll) {
+                        const origElement = uniqueElements[href].querySelector('td.a1 a');
+                        var spanElement = document.createElement('span');
+                        spanElement.className = 'l r';
+                        spanElement.textContent = '\u200C';
+                        origElement.appendChild(spanElement);
+                        origElement.appendChild(anchorElement.children[1]);
+                    }
+                    row.parentNode.removeChild(row);
+                }
+            }
+        });
+
+        // clean the text
+        const movieName = document.getElementsByClassName('header')[0].querySelector('h2').textContent.trim().split('\n')[0];
+        const newRows = document.getElementsByTagName('table')[0].querySelectorAll('tr');
+        newRows.forEach((row) => {
+            const anchorElement = row.querySelector('td.a1 a');
+            if (anchorElement) {
+                const spans = anchorElement.querySelectorAll('span:nth-child(even)');
+                if (spans) {
+                    spans.forEach((span) => {
+                        var title = span.innerText;
+                        title = title.replace(/\./g, ' ');
+                        title = title.replace(RegExp(movieName, 'gi'), '').trim();
+                        title = title.replace(/(19|20)\d{2}/gm, '');
+                        title = title.replace(/\(\s*\)/g, '').trim();
+                        title = title.replace(/ {2,}/g, ' ').trim();
+                        title = title.replace(/[\[\]]/g, '').trim();
+                        span.innerText = title ? title : movieName;
+                    });
+                }
+            }
+        });
+    }
+
+    function observeDOM() {
+        var targetNode = document.body;
+        var config = { childList: true, subtree: true };
+        var callback = function(mutationsList, observer) {
+            if (document.querySelector('table')) {
+                observer.disconnect();
+                removeDuplicates();
+            }
+        };
+        var observer = new MutationObserver(callback);
+        observer.observe(targetNode, config);
+    }
+    observeDOM();
+})();
